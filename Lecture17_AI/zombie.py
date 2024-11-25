80beb449a1559a950e21469765f94efe39e3f917
Ieon <yieon1028@gmail.com>
2024-11-25 22:37:25 +0900

    기초 설정

--- Lecture17_AI/zombie.py
+++ Lecture17_AI/zombie.py
@@ -130,21 +130,11 @@ class Zombie:
     def compare_balls(self):
         pass
 
+    def flee_from_boy(self, r=0.5):
+        pass
+
+    def is_far_from_boy(self, r=10):
+        pass
+
     def build_behavior_tree(self):
-        a1 = Action('Set target location', self.set_target_location, 1000, 1000)
-        a2 = Action('Move to', self.move_to)
-        root = move_to_target_location = Sequence('Move to target location', a1, a2)
-
-        a3 = Action('Set random location', self.set_random_location)
-        root = wander = Sequence('Wander', a3, a2)
-
-        c1 = Condition('소년이 근처에 있는가?', self.is_boy_nearby, 7)
-        a4 = Action('소년한테 접근', self.move_to_boy)
-        root = chase_boy = Sequence('소년을 추적', c1, a4)
-
-        root = chase_or_flee = Selector('추적 또는 배회', chase_boy, wander)
-
-        # a5 = Action('순찰 위치 가져오기', self.get_patrol_location)
-        # root = patrol = Sequence('순찰', a5, a2)
-
         self.bt = BehaviorTree(root)
